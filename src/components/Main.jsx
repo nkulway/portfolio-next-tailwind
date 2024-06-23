943e83b69abe176c39a9d36f8fdbb18e8cac83d3
“Nick” <nkulway@gmail.com>
2024-06-23 18:51:48 -0400

    fix syntax bug

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
             Lets build somehting together
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I'm <span className="text-[#5651e5]">Nick</span>
+            Hi, I&apos;m <span className="text-[#5651e5]">Nick</span>
           </h1>
           <h1 className="py-2 text-gray-700">A Software Developer</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">

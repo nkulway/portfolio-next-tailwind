747b038c19f3f94d03df6583aeb75f931268720e
“Nick” <nkulway@gmail.com>
2024-06-23 18:51:37 -0400

    add new skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -104,6 +104,21 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
+                  src="/../public/assets/skills/java.png"
+                  alt="Java"
+                  width="64"
+                  height="64"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Java</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
                   src="/../public/assets/skills/css.png"
                   alt="CSS"
                   width="64"

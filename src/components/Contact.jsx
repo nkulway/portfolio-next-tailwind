7034919f74a1d94e88eea63f63a0beaab4906d35
“Nick” <nkulway@gmail.com>
2024-09-11 16:36:52 -0400

    fix bug with click

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,13 +9,13 @@ const Contact = () => {
   return (
     <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full ">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5] text-center">
           Contact
         </p>
-        <h2 className="py-4">Get In Touch</h2>
-        <div className="grid lg:grid-cols-5 gap-8">
+        <h2 className="py-4 text-center">Get In Touch</h2>
+        <div className="flex justify-center">
           {/* left */}
-          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+          <div className="w-full lg:w-1/3 h-full shadow-xl shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full">
               <div>
                 <img
@@ -35,109 +35,36 @@ const Contact = () => {
               <div>
                 <p className="uppercase pt-8">Connect With Me</p>
                 <div className="flex items-center justify-between py-4">
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <a
-                      href="https://www.linkedin.com/in/nkulway/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedinIn />
-                    </a>
-                  </div>
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <a
-                      href="https://github.com/nkulway"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaGithub />
-                    </a>
-                  </div>
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <a href="mailto:nkulway@gmail.com?subject=Inquiry%20from%20Your%20Portfolio&body=Hello%20[Nick%20Kulway],%0D%0A%0D%0AI%20am%20interested%20in%20discussing%20[topic]%20with%20you.%0D%0A%0D%0ARegards,%0D%0A[Nick%20Kulway]">
-                      <AiOutlineMail />
-                    </a>
-                  </div>
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <a href="tel:4045636282">
-                      <BsFillPersonLinesFill />
-                    </a>
-                  </div>
+                  <a
+                    href="https://www.linkedin.com/in/nkulway/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="rounded-full shadow-lg shadow-gray-400 w-16 h-16 flex items-center justify-center cursor-pointer hover:scale-110 ease-in duration-300"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                  <a
+                    href="https://github.com/nkulway"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="rounded-full shadow-lg shadow-gray-400 w-16 h-16 flex items-center justify-center cursor-pointer hover:scale-110 ease-in duration-300"
+                  >
+                    <FaGithub />
+                  </a>
+                  <a
+                    href="mailto:nkulway@gmail.com?subject=Inquiry%20from%20Your%20Portfolio&body=Hello%20[Nick%20Kulway],%0D%0A%0D%0AI%20am%20interested%20in%20discussing%20[topic]%20with%20you.%0D%0A%0D%0ARegards,%0D%0A[Nick%20Kulway]"
+                    className="rounded-full shadow-lg shadow-gray-400 w-16 h-16 flex items-center justify-center cursor-pointer hover:scale-110 ease-in duration-300"
+                  >
+                    <AiOutlineMail />
+                  </a>
+                  <a
+                    href="tel:4045636282"
+                    className="rounded-full shadow-lg shadow-gray-400 w-16 h-16 flex items-center justify-center cursor-pointer hover:scale-110 ease-in duration-300"
+                  >
+                    <BsFillPersonLinesFill />
+                  </a>
                 </div>
               </div>
-            </div>
-          </div>
-
-          {/* right */}
-
-          <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
-            <div className="p-4">
-              <form>
-                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-                  <div className="flex flex-col">
-                    <label htmlFor="name" className="uppercase text-sm py-2">
-                      Name
-                    </label>
-                    <input
-                      id="name"
-                      autoComplete="on"
-                      className="border-2 rounded-lg p-3 border-gray-300"
-                      type="text"
-                      name="name"
-                    />
-                  </div>
-
-                  <div className="flex flex-col">
-                    <label htmlFor="phone" className="uppercase text-sm py-2">
-                      Phone Number
-                    </label>
-                    <input
-                      id="phone"
-                      autoComplete="on"
-                      className="border-2 rounded-lg p-3 flex border-gray-300"
-                      type="text"
-                      name="phone"
-                    />
-                  </div>
-                </div>
-                <div className="flex flex-col py-2">
-                  <label htmlFor="email" className="uppercase text-sm py-2">
-                    Email
-                  </label>
-                  <input
-                    id="email"
-                    autoComplete="on"
-                    className="border-2 rounded-lg p-3 flex border-gray-300"
-                    type="email"
-                    name="email"
-                  />
-                </div>
-                <div className="flex flex-col py-2">
-                  <label htmlFor="subject" className="uppercase text-sm py-2">
-                    Subject
-                  </label>
-                  <input
-                    id="subject"
-                    className="border-2 rounded-lg p-3 flex border-gray-300"
-                    type="text"
-                    name="subject"
-                  />
-                </div>
-                <div className="flex flex-col py-2">
-                  <label htmlFor="message" className="uppercase text-sm py-2">
-                    Message
-                  </label>
-                  <textarea
-                    id="message"
-                    className="border-2 rounded-lg p-3 border-gray-300"
-                    rows="10"
-                    name="message"
-                  ></textarea>
-                </div>
-                <button className="w-full p-4 text-gray-100">
-                  Send Message
-                </button>
-              </form>
             </div>
           </div>
         </div>
@@ -150,6 +77,7 @@ const Contact = () => {
         </div>
       </div>
     </div>
+
   )
 }
 

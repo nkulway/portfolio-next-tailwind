92ce7da0195804c02c132b0eac1a6103316eb310
Nick Kulway <75750859+nkulway@users.noreply.github.com>
2025-02-28 14:41:32 -0500

    Add skill (#7)
    * update href
    
    * add skill
    
    * update image
    
    ---------
    
    Co-authored-by: “Nick” <“nkulway@gmail.com”>

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -239,6 +239,21 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
+                  src="/assets/skills/kube.png"
+                  alt="Kubernetes"
+                  width="64"
+                  height="64"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Kubernetes</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
                   src="/assets/skills/github1.png"
                   alt="Github"
                   width="64"

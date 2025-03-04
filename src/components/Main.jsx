54394aa34c6e526facd36302a6adb3ef738130ca
Nick Kulway <75750859+nkulway@users.noreply.github.com>
2025-03-03 19:03:46 -0500

    typo (#8)
    Co-authored-by: “Nick” <“nkulway@gmail.com”>

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,21 +6,16 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 const Main = () => {
   return (
     <div id="home" className="w-full h-screen text-center">
-      <div className="max-w[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            Lets build somehting together
+            Lets build something together
           </p>
           <h1 className="py-4 text-gray-700">
             Hi, I&apos;m <span className="text-[#5651e5]">Nick</span>
           </h1>
           <h1 className="py-2 text-gray-700">A Software Developer</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-            {/* As a software developer with a focus on creating and upholding
-            exceptional digital experiences, my current area of specialization
-            is centered on constructing a comprehensive automated testing suite
-            from scratch at Bakkt. Meanwhile, I am also dedicated to expanding
-            my knowledge of emerging technologies. */}
             I specialize in creating and maintaining outstanding digital
             experiences as a software developer. Presently, my attention is on
             establishing a comprehensive automated testing suite from scratch at
